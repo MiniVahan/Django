25f28194bd2e0f56cff5616e360b3cf2a5eba8c0
Mamikon Papikyan <mamikon_papikyan@instigatedesign.com>
2021-06-25 23:47:00 +0400

    initial commit

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# pages/tests.py
 
-# Create your tests here.
+from django.test import TestCase, SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
