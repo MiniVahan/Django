e01150614aa370a0b0d11471a9f6514583d6e5bc
Mamikon Papikyan <mamikon_papikyan@instigatedesign.com>
2021-06-25 21:00:05 +0400

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+# pages/views.py
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homePageView(request):
+    return HttpResponse('Hello, World!')
+
+# Create your views here.
